319326ced204b0222eb4e5ab8ec2dc072ef5d7c5
Akorede Adewole <43486426+Akorex@users.noreply.github.com>
2026-05-13 19:56:39 +0100

    feat: added graceful shutdown and timeout

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,13 @@
 package main
 
 import (
+	"context"
 	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/Akorex/caronago/internal/config"
 	"github.com/Akorex/caronago/internal/database"
@@ -14,7 +20,7 @@ func main(){
 	cfg := config.LoadConfig()
 
 	r := gin.New()
-	r.Use(gin.Logger())
+	r.Use(gin.Logger(), gin.Recovery())
 
 	database.Connect(cfg.DBUrl)
 	
@@ -26,6 +32,41 @@ func main(){
 		port = "8080"
 	}
 
-	log.Println("Server running on port", port)
-	r.Run(":" + port)
+	srv := &http.Server{
+		Addr: ":" + port,
+		Handler: r,
+		ReadTimeout:  5 * time.Second,  // Max time to read the incoming request
+		WriteTimeout: 10 * time.Second, // Max time to write the outgoing response
+		IdleTimeout:  120 * time.Second, // Max time a connection can stay open without doing anything
+	}
+
+
+	// start the server in a goroutine 
+	go func(){
+		log.Printf("Server running on port %s", port)
+
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed{
+			log.Fatalf("Server failed to start :%v\n", err)
+		}
+	}()
+
+
+	// wait for an interrupt signal
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<- quit
+
+	log.Println("Shutdown signal received. Draining connections...")
+
+	// create a 5-second timeout context for shutdown
+	ctx, cancel := context.WithTimeout(context.Background(), 5 * time.Second)
+	defer cancel()
+
+	// attempt graceful shutdown
+	if err := srv.Shutdown(ctx); err != nil{
+		log.Fatal("Server forced to shutdown forcefully: ", err)
+	}
+
+	log.Println("Server exited cleanly.")
+
 }
